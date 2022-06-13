c6d7cd827ed869f1f45966b1e763808f26b77651
gaeun <knospe1@naver.com>
2022-06-13 23:46:40 +0900

    test(문자열 계산기): 테스트코드 작성

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,14 +1,31 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class CalculatorTest {
 
+    Calculator calculator = new Calculator();
 
     @Test
-    void calculator() {
-        Calculator calculator = new Calculator();
+    void calculateStartWithPositiveNum() {
         String input = "2 + 3 * 4 / 2";
-        String result = calculator.calculate(input);
-        Assertions.assertEquals(result, "10");
+        int result = calculator.calculate(input);
+        Assertions.assertEquals(result, 10);
     }
+
+    @Test
+    void calculateStartWithNegativeNum() {
+        String input = "-19 + 7 / 4";
+        int result = calculator.calculate(input);
+        Assertions.assertEquals(result, -3);
+    }
+
+    @Test
+    void calculateIfInputOnlyOneNum() {
+        String input = "3";
+        int result = calculator.calculate(input);
+        Assertions.assertEquals(result, 3);
+    }
+
 }
