63e9951553d7b8e882dcdc67f83b36067f25fa67
gaeun <knospe1@naver.com>
2022-06-13 23:46:28 +0900

    feat(문자열 계산기): 계산기 기능 구현

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,12 +1,33 @@
+import java.util.Scanner;
 
 public class Calculator {
 
-    public String calculate(String input) {
-        String result = "";
+    public int calculate(String input) {
+        String[] inputs = input.split(" ");
+        int result = Integer.parseInt(inputs[0]);
+        int number = result;
+
+        for (int i = 2; i < inputs.length; i+=2) {
+            number = Integer.parseInt(inputs[i]);
+            if (inputs[i - 1].equals("+")) {
+                result += number;
+            } else if (inputs[i - 1].equals("-")) {
+                result -= number;
+            } else if (inputs[i - 1].equals("*")) {
+                result *= number;
+            } else if (inputs[i - 1].equals("/")) {
+                result /= number;
+            }
+        }
         return result;
     }
 
     public static void main(String[] arr) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(input);
+        System.out.println(result);
     }
 }
