d8abdeb670952d437eb431b45fc00737c8b046e9
gaeun <knospe1@naver.com>
2022-06-16 21:12:06 +0900

    test(baseball): 스트라이크, 볼, 포볼에 대한 테스트코드 작성

---
+++ src/test/java/BaseballGameTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+public class BaseballGameTest {
+
+    @Test
+    void whenInputMatchOneNumWithRightPosition() {
+
+    }
+
+    @Test
+    void whenInputMatchOneNumWithWrongPosition() {
+
+    }
+
+    @Test
+    void whenInputNotMatchAnyNum() {
+
+    }
+}
